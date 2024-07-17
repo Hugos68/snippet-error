f7f1da0aae4a0a8eccf65a70a306b4307cd70d59
hugos68 <hugokorteapple@gmail.com>
2024-07-17 22:15:29 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Foo from './Foo.svelte';
+</script>
+
+<Foo foo="">
+    {#snippet snippetOne()}
+    {/snippet}
+    <label>Test</label>
+    {#snippet snippetTwo()}
+    {/snippet}
+</Foo>
